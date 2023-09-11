d697a4da7c181e5438b19630daa10689b87cfc17
jayxd0909 <61066097+jayxd0909@users.noreply.github.com>
2023-09-11 15:08:20 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -4,31 +4,57 @@ var http = require('http'),
 
 /* Global variables */
 var listingData, server;
+var http = require('http');
+var url = require('url');
+var fileSystem = require('fs');
 
-var requestHandler = function(request, response) {
-  /*Investigate the request object. 
-    You will need to use several of its properties: url and method
-  */
-  //console.log(request);
-
-  /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   
-    HINT: Explore mdn web docs for resources on how to use javascript.
-    Helpful example: if-else structure- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
-
+var requestHandler = function (request, response) {
+    /*Investigate the request object. 
+      You will need to use several of its properties: url and method
     */
+    //console.log(request);
+
+    var pathname = url.parse(request.url).pathname;
+    if (pathname == '/listings') {
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+
+        fileSystem.readFile('listings.json', 'utf8', Error);
+
+        listingData.on('data', writeCallback);
+        listingData.on('close', closeCallback);
+
+        function writeCallback(data) {
+            response.write(data);
+        }
+
+        function closeCallback() {
+            response.end();
+        }
+    }
+    else {
+        throw (
+            new Error('404 Not Found')
+        );
+    }
+
+    /*
+      Your request handler should send listingData in the JSON format as a response if a GET request 
+      is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+  
+      HINT: Explore the request object and its properties 
+      HINT: Explore the response object and its properties
+      https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
+      
+      HINT: Explore how callback's work 
+      http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
+      
+      HINT: Explore the list of MIME Types
+      https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
+     
+      HINT: Explore mdn web docs for resources on how to use javascript.
+      Helpful example: if-else structure- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
+  
+      */
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
@@ -47,14 +73,18 @@ fs.readFile('listings.json', 'utf8', function(err, data) {
     /*this resource gives you an idea of the general format err objects and Throwing an existing object.
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw#throwing_an_existing_object
    */
-  
 
    //Save the data in the listingData variable already defined
-  
+
+    listingData = fs.createReadStream('listings.json');
 
   //Creates the server
-  
+
+    server = http.createServer(requestHandler);
+
   //Start the server
 
-
+    server.listen(8080, function () {
+        console.log('server listening on: http://localhost:8080');
+    });
 });
